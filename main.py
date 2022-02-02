291b2b725b552cc095ff7160e271a524dae4989a
José María Amuedo Laínez <josemaria.amuedo@ec2ce.com>
2022-02-02 18:27:50 +0100

    Funcion principal

--- main.py
+++ main.py
@@ -93,3 +93,83 @@ def ConclusionMensaje(result_mes):
     else:
         mensaje = 'sin perdidas.'
     return mensaje
+
+def func_pp():
+    # leo mi archivo csv
+    try:
+        df = pd.read_csv('finanzas2020.csv', sep='\t')
+        # genero una lista con los meses que aparecen en el archivo
+        meses = list(df.columns)
+    except IOError as err:
+        print("El fichero no puede ser encontrado")
+    else:
+        print("Fichero leido correctammente")
+
+    # declaro listas vacias
+    lista_balance=[]
+    lista_gasto=[]
+    lista_ingreso=[]
+    # Accedemos al dataframe para ir recorriendo los datos por columna
+    for column in df:
+        try:
+            contenido = df[column]
+            cont_lst = list(contenido.values)
+            result_mes=[]
+            # Valoramos si mi archivo tien 12 columnas y si tiene contenido cada mes
+            if len(contenido)==12:
+                    raise FicheroAnual
+        except FicheroAnual:
+            print("Tu archivo no tiene 12 columnas")
+        try:
+            if len(cont_lst)==0:
+                raise HayContenido
+        except HayContenido:
+            print("No hay contenido para el mes")
+        # Recorro la lista creada que contiene los valores de un mes
+        for i in cont_lst:
+            # Primero convierto el valor a string por dos motivos:
+            # 1.-Me aseguro que mi valor parte de ser string
+            # 2.-Para poder manejar todos los casos a tratar
+
+            i = str(i)
+            # Segundo intento eliminar las comillas que pueden darme problemas
+            # para manerja mi valor
+            i = i.replace("'", "")
+            # Tercero convierto a entero, si no puedo lo igualo a cero
+            try:
+                i = int(i)
+            except:
+                i = 0
+            # Cuarto, voy almacenando mi valor tratado a una lista
+            result_mes.append(i)
+        # Genero tres variables nuevas para almacenar el resultado de aplicar las funciones
+        b = Balance(result_mes)
+        g = Gastos(result_mes)
+        h = Ingresos(result_mes)
+        # Almaceno todo en una lista para poder darle salida mas tarde
+        lista_balance.append(b)
+        # print("En el mes de ", column ,": ", ConclusionMensaje(result_mes))
+        lista_gasto.append(g)
+        lista_ingreso.append(h)
+
+
+    # Genero nuevos dataframes que contienen el total de ingresos, gastos y su Balance
+    # de cada mes a lo largo del año
+    df_ingreso =  pd.DataFrame([lista_ingreso], columns=meses)
+    df_gasto = pd.DataFrame([lista_gasto], columns=meses)
+    df_balance = pd.DataFrame([lista_balance], columns=meses)
+    # Respondo a las preguntas accediendo a mis nuevos dataframes
+    # Obtengo tanto el dato com el mes con mayor ganancia
+    maxs_b=df_balance.max(axis=1)
+    mes_maxb = df_balance.idxmax(axis=1)
+    # Obtengo tanto el dato com el mes con mayor perdida
+    mins_B = df_balance.min(axis=1)
+    mes_minb = df_balance.idxmin(axis=1)
+    # Obtengo tanto el dato concreto del mayor gasto como su mes
+    maxs_g=df_gasto.min(axis=1)
+    mes_maxg = df_gasto.idxmin(axis=1)
+    # Obtengo tanto el dato concreto del mayor ingreso como su mes
+    mins_i = df_ingreso.max(axis=1)
+    mes_mini = df_ingreso.idxmax(axis=1)
+    # Obtengo la media de los gastos del año
+    mean_g = df_gasto.mean(axis=1)
